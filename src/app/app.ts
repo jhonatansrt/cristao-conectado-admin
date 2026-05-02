368984e0baa2f4b232168adf1d05e747debc81b4
marciohubees <marcio@netpark.com.br>
2026-05-02 11:46:32 -0300

    fix: add pastes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
-import { Component, ViewChild, ViewContainerRef, inject } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Component, inject, ViewChild, ViewContainerRef } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { Container } from './util/container.service';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [IonApp, IonRouterOutlet],
 })
 export class App {
   @ViewChild('main', { static: true, read: ViewContainerRef })
