fedfbe336604fcda588a5e731b083c3ee372d465
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-02 11:27:13 -0300

    refactor: update root app shell with Ionic outlet container

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, ViewContainerRef, inject } from '@angular/core';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
+import { Container } from './util/container.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [IonApp, IonRouterOutlet],
 })
 export class App {
-  protected readonly title = signal('cristao-conectado-admin');
+  @ViewChild('main', { static: true, read: ViewContainerRef })
+  private vcr!: ViewContainerRef;
+
+  private readonly container = inject(Container);
+
+  ngOnInit(): void {
+    this.container.vcr = this.vcr;
+  }
 }
