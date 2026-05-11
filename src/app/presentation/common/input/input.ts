73d51d5fc254e689305f8f90f878f9e3ecb42076
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-11 11:30:38 -0300

    style: align input component scss with nested structure

---
+++ src/app/presentation/common/input/input.ts
@@ -0,0 +1,21 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.scss',
+})
+export class InputComponent {
+  public readonly label = input<string>('');
+  public readonly value = input<string>('');
+  public readonly placeholder = input<string>('');
+  public readonly errorMessage = input<string>('');
+
+  public readonly valueChange = output<string>();
+
+  public onInput(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.valueChange.emit(target.value);
+  }
+}
