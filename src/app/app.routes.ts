6af2f29a44899aad1edc5054d9936b3ec0275227
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-12 15:23:31 -0300

    feat: add template root route with church child

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 
 import { namedRoutes } from './named-routes';
 import { Login } from './presentation/auth/login/login';
+import { Church } from './presentation/church/church';
+import { Template } from './presentation/template/template';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: namedRoutes.default,
     pathMatch: 'full',
+  },
+  {
+    path: namedRoutes.template,
+    component: Template,
+    children: [
+      {
+        path: '',
+        redirectTo: namedRoutes.church,
+        pathMatch: 'full',
+      },
+      {
+        path: namedRoutes.church,
+        component: Church,
+      },
+    ],
   },
   {
     path: namedRoutes.login,
---
+++ src/app/presentation/church/church.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-church',
+  imports: [],
+  templateUrl: './church.html',
+  styleUrl: './church.scss',
+})
+export class Church {}
