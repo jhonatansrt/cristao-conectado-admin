898f7f7d33e6646e5bf381405ad4589398d576fe
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-12 11:32:25 -0300

    refactor(modal): allow footer actions via projected content

--- src/app/presentation/auth/create-account/create-account.ts
+++ src/app/presentation/auth/create-account/create-account.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../common/button/button.component';
 import { InputComponent } from '../../common/input/input';
 import { ModalComponent } from '../../common/modal/modal.component';
 
 @Component({
   selector: 'app-create-account',
-  imports: [InputComponent, ModalComponent],
+  imports: [InputComponent, ModalComponent, ButtonComponent],
   templateUrl: './create-account.html',
   styleUrl: './create-account.scss',
 })
