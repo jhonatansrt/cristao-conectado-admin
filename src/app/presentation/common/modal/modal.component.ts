6524960bb17370b786bf7fe216c111d6cd62eea4
marciohubees <marcio@netpark.com.br>
2026-05-12 14:09:39 -0300

    fix: adjustments

--- src/app/presentation/common/modal/modal.component.ts
+++ src/app/presentation/common/modal/modal.component.ts
@@ -1,10 +1,12 @@
 import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   standalone: true,
+  imports: [MatIconModule],
 })
 export class ModalComponent implements OnInit {
   @Input() public title: string | undefined;
