5a38cb5f04391a050bedf2e6eb9c9111635cb66c
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-12 17:12:30 -0300

    refactor(menu): extract profile preview component and use theme vars

---
+++ src/app/presentation/common/menu/profile-preview/profile-preview.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-profile-preview',
+  imports: [MatIconModule],
+  templateUrl: './profile-preview.html',
+  styleUrl: './profile-preview.scss',
+})
+export class ProfilePreview {}
