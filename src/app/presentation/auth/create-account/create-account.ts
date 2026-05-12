01ca4068f7c128a971fda847aad7adf2a34dc0f3
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-11 22:06:02 -0300

    refactor(auth): remove create-account route wiring

---
+++ src/app/presentation/auth/create-account/create-account.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '../../common/input/input';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [InputComponent],
+  templateUrl: './create-account.html',
+  styleUrl: './create-account.scss',
+})
+export class CreateAccount {}
