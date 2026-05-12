4e307cfd1ac19733da910d8814128c3204c76eb9
marciohubees <marcio@netpark.com.br>
2026-05-11 21:03:10 -0300

    fix: adjustments

---
+++ src/app/presentation/common/modal/modal.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  standalone: true,
+  imports: [],
+})
+export class ModalComponent implements OnInit {
+  @Input() public title: string | undefined;
+  public indexModal = 1;
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    this.indexModal = document.querySelectorAll('.background-modal').length;
+  }
+
+  public closeModal() {
+    const selector = this.el.nativeElement.parentElement.tagName.toLowerCase();
+
+    document.querySelector('#modal' + this.indexModal)?.classList.add('close-background-modal');
+
+    document
+      .querySelector('#modal' + this.indexModal + ' .content-modal')
+      ?.classList.add('close-content-modal');
+
+    setTimeout(() => {
+      document.querySelector(selector + ':nth-last-of-type(1)')?.remove();
+    }, 100);
+  }
+}
