f47356260863e7a6473d01bc37678eb5b8ea281c
marciohubees <marcio@netpark.com.br>
2026-05-11 22:11:53 -0300

    fix: adjustments

--- src/app/presentation/auth/create-account/create-account.ts
+++ src/app/presentation/auth/create-account/create-account.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../common/input/input';
+import { ModalComponent } from '../../common/modal/modal.component';
 
 @Component({
   selector: 'app-create-account',
-  imports: [InputComponent],
+  imports: [InputComponent, ModalComponent],
   templateUrl: './create-account.html',
   styleUrl: './create-account.scss',
 })
