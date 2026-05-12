b16080ee582160e57c995e347298272570a5586b
marciohubees <marcio@netpark.com.br>
2026-05-12 17:04:39 -0300

    fix: template

--- src/app/presentation/template/template.ts
+++ src/app/presentation/template/template.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { Header } from '../common/header/header';
+import { Menu } from '../common/menu/menu';
 
 @Component({
   selector: 'app-template',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, Menu],
   templateUrl: './template.html',
   styleUrl: './template.scss',
 })
