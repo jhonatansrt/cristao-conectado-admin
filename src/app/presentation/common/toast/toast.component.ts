79c096e9d7075c84615e62f2428f449fc2bc2efc
marciohubees <marcio@netpark.com.br>
2026-05-13 17:08:01 -0300

    fix: toast

--- src/app/presentation/common/toast/toast.component.ts
+++ src/app/presentation/common/toast/toast.component.ts
@@ -1,26 +1,18 @@
 import { Component, Input } from '@angular/core';
-import { IonIcon } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import {
-  closeOutline,
-  checkmarkCircleOutline,
-  alertCircleOutline,
-} from 'ionicons/icons';
 import { ToastService } from './toast.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
   standalone: true,
-  imports: [IonIcon],
+  imports: [MatIconModule],
 })
 export class ToastComponent {
   @Input() success: boolean = true;
   @Input() title: string | undefined;
   @Input() message: string | undefined;
 
-  constructor(public toastService: ToastService) {
-    addIcons({ closeOutline, checkmarkCircleOutline, alertCircleOutline });
-  }
+  constructor(public toastService: ToastService) {}
 }
