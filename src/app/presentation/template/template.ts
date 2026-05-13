922795bf12680b9d17379ec2253670fc670d8793
marciohubees <marcio@netpark.com.br>
2026-05-12 22:57:30 -0300

    fix: menu

--- src/app/presentation/template/template.ts
+++ src/app/presentation/template/template.ts
@@ -1,9 +1,5 @@
-import { Component, effect, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { filter, map, startWith } from 'rxjs';
-
-import { HeaderStore } from '../../application/header/header-store';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from '../common/header/header';
 import { Menu } from '../common/menu/menu';
 
@@ -13,33 +9,4 @@ import { Menu } from '../common/menu/menu';
   templateUrl: './template.html',
   styleUrl: './template.scss',
 })
-export class Template {
-  private readonly navController = inject(Router);
-  private readonly activatedRoute = inject(ActivatedRoute);
-  private readonly headerStore = inject(HeaderStore);
-
-  private readonly currentTitle = toSignal(
-    this.navController.events.pipe(
-      filter((event) => event instanceof NavigationEnd),
-      map(() => this.getCurrentRouteTitle()),
-      startWith(this.getCurrentRouteTitle()),
-    ),
-    { initialValue: this.getCurrentRouteTitle() },
-  );
-
-  public constructor() {
-    effect(() => {
-      this.headerStore.setTitle(this.currentTitle());
-    });
-  }
-
-  private getCurrentRouteTitle(): string {
-    let route = this.activatedRoute;
-
-    while (route.firstChild) {
-      route = route.firstChild;
-    }
-
-    return (route.snapshot.data?.['title'] as string | undefined) ?? 'Igreja';
-  }
-}
+export class Template {}
