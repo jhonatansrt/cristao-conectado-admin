3c49428f9abc1ae60510534da86bb8d6d1ba1317
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-12 22:38:02 -0300

    refactor: switch menu navigation to nav controller logic

---
+++ src/app/presentation/playlists/playlists.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists',
+  imports: [],
+  templateUrl: './playlists.html',
+  styleUrl: './playlists.scss',
+})
+export class Playlists {}
