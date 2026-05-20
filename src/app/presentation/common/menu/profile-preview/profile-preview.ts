226b3e58c0f9fb56d84a83bc2fd676414363643f
marciohubees <marcio@netpark.com.br>
2026-05-20 19:42:38 -0300

    fix: adjustments

--- src/app/presentation/common/menu/profile-preview/profile-preview.ts
+++ src/app/presentation/common/menu/profile-preview/profile-preview.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthStore } from '../../../../application/auth/auth-store';
+import { CapitalizeNamePipe } from '../../../../pipes/capitalize-name.pipe';
 
 @Component({
   selector: 'app-profile-preview',
-  imports: [MatIconModule],
+  imports: [MatIconModule, CapitalizeNamePipe],
   templateUrl: './profile-preview.html',
   styleUrl: './profile-preview.scss',
 })
-export class ProfilePreview {}
+export class ProfilePreview {
+  private readonly authStore = inject(AuthStore);
+  public readonly userLogged = this.authStore.getUserLogged();
+}
