cb85acfc132331bdccd86c92b7f6d362559ae242
jhonatansrt <44975271+jhonatansrt@users.noreply.github.com>
2026-05-20 17:14:57 -0300

    Adiciona empty case na tela de avisos

--- src/app/presentation/notices/notices.ts
+++ src/app/presentation/notices/notices.ts
@@ -1,17 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
 import { TableColumn, TableRow, TableStore } from '../../application/table/table-store';
 import { TableComponent } from '../common/table/table.component';
 import { NoticesService } from '../../application/notices/notices-service';
+import { EmptyCaseComponent } from '../common/empty-case/empty-case.component';
 
 @Component({
   selector: 'app-notices',
-  imports: [TableComponent],
+  imports: [TableComponent, EmptyCaseComponent],
   templateUrl: './notices.html',
   styleUrl: './notices.scss',
 })
 export class Notices implements OnInit {
   private readonly tableStore = inject(TableStore<TableRow>);
   private noticesService = inject(NoticesService);
+  protected readonly isLoading = this.tableStore.isLoading();
 
   constructor() {
     const columns: TableColumn[] = [
@@ -26,12 +29,21 @@ export class Notices implements OnInit {
     this.getNotices();
   }
 
-  private getNotices() {
-    this.noticesService.getNotices().subscribe({
-      next: (rows) => {
-        this.tableStore.setRows(rows);
-      },
-      error: () => {},
-    });
+  private getNotices(): void {
+    this.tableStore.setLoading(true);
+
+    this.noticesService
+      .getNotices()
+      .pipe(finalize(() => this.tableStore.setLoading(false)))
+      .subscribe({
+        next: (rows) => {
+          this.tableStore.setRows(rows);
+        },
+        error: () => {},
+      });
+  }
+
+  protected hasNotices(): boolean {
+    return this.tableStore.hasRows();
   }
 }
