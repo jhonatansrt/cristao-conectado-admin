277124c2e9bf58817370c7b9e1f82a9d68cc0320
marciohubees <marcio@netpark.com.br>
2026-06-09 20:42:22 -0300

    fix: adjustments

--- src/app/presentation/members/reports/reports.ts
+++ src/app/presentation/members/reports/reports.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Summary } from './summary/summary';
+import { ActionBarStore } from '../../../application/action-bar/action-bar-store';
+import { Router } from '@angular/router';
+import { namedRoutes } from '../../../named-routes';
 
 @Component({
   selector: 'app-reports',
@@ -7,4 +10,29 @@ import { Summary } from './summary/summary';
   templateUrl: './reports.html',
   styleUrl: './reports.scss',
 })
-export class Reports {}
+export class Reports {
+  private readonly actionBarStore = inject(ActionBarStore);
+  private readonly navController = inject(Router);
+
+  ngOnInit() {
+    this.setButtonsActions();
+  }
+
+  ngOnDestroy(): void {
+    this.actionBarStore.clearButtonsActions();
+  }
+
+  private setButtonsActions() {
+    this.actionBarStore.setButtonsActions([
+      {
+        btnClass: 'btn-primary',
+        label: 'Exibir lista',
+        onClick: this.openListMembers,
+      },
+    ]);
+  }
+
+  private readonly openListMembers = (): void => {
+    this.navController.navigate([namedRoutes.members]);
+  };
+}
