9f3b78b5aaacc69e8dd2280e5e7508b62b3681fa
marciohubees <marcio@netpark.com.br>
2026-06-26 10:54:53 -0300

    fix: adjustments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseURL: 'https://2271-2804-1b2-d082-283a-157-93a7-c45-6fd8.ngrok-free.app',
+  apiBaseURL: 'https://07bb-2804-1b2-d083-ab8-d88-1be7-c302-f94c.ngrok-free.app',
 };
