af475b23892f478a1dd59179e854b0d439f9bec1
CassiaHelenSrt <cassiahelen9213@gmail.com>
2026-07-07 11:51:10 -0300

    getClass

---
+++ src/app/domain/ebd/dto/get-ebd-class.dto.ts
@@ -0,0 +1,3 @@
+export interface GetEbdClassParamsDTO {
+  church_id: string;
+}
--- src/app/infrastructure/ebd/ebd-class-repository.ts
+++ src/app/infrastructure/ebd/ebd-class-repository.ts
@@ -1,10 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { IEbdClassRepository } from '../../domain/ebd/ebd-class.repository';
 import { CreateEbdClassDTO } from '../../domain/ebd/dto/create-ebd-class.dto';
 import { CreateEbdClassResponseDTO } from '../../domain/ebd/dto/create-ebd-class-response.dto';
+import { GetEbdClassParamsDTO } from '../../domain/ebd/dto/get-ebd-class.dto';
+import { EbdClass } from '../../domain/ebd/entities/ebd-class.entity';
 
 @Injectable({
   providedIn: 'root',
@@ -23,5 +25,11 @@ export class EbdClassRepository implements IEbdClassRepository {
     };
 
     return this.httpClient.post<CreateEbdClassResponseDTO>(url, body);
+  }
+
+  public getEbdClass(props: GetEbdClassParamsDTO) {
+    const url = `${environment.apiBaseURL}/ebd/class/church/${props.church_id}`;
+
+    return this.httpClient.get<EbdClass[]>(url);
   }
 }
